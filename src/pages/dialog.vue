4368320964c5dad127532c857cce210521da9cb0
vyacheslav.nagel <v9gel@mail.ru>
2021-09-14 22:58:18 +0700

    + dialog page

---
+++ src/pages/dialog.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useUserStore } from '~/stores/user'
+
+const user = useUserStore()
+const name = ref(user.savedName)
+
+const router = useRouter()
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <div>
+    kggkj
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: default
+</route>
